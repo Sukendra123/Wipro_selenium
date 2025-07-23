3aa6e8bf71c2ab90df755aa763334d20376eb28b
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-23 12:07:42 +0530

    Create Abstractionprogramsimple.java

---
+++ ProgramsDay4/Abstractionprogramsimple.java
@@ -0,0 +1,30 @@
+package ProgramsDay4;
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
+public class Abstractionprogramsimple {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape rectangle = new Rectangle();
+
+        circle.draw();
+        rectangle.draw();
+    }
+}
+
