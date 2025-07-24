8f373ad3a27c3cecccd6a2dec111f85efcb12f1f
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-24 18:18:41 +0530

    Create Vehicle_main.java

---
+++ wipro_day4/Vehicle_main.java
@@ -0,0 +1,13 @@
+package wipro_day4;
+
+public class Vehicle_main {
+	public static void main(String[] args) {
+		Vehicle_type v = new Car();
+		Vehicle_type B = new Bike();
+		v.speed();
+		v.brand();
+		B.speed();
+		B.brand();
+	}
+
+}
