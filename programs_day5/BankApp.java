748f20e7fea32e31fd8d10a8c221d2763c22f030
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-25 19:15:12 +0530

    Create BankApp.java

---
+++ programs_day5/BankApp.java
@@ -0,0 +1,16 @@
+package programs_day5;
+
+
+
+
+public class BankApp {
+    public static void main(String[] args) {
+        Bank account = new SavingAccount(1000);
+
+        account.displayAccountType();           // default override
+        account.executeTransaction("deposit", 0);   // deposit
+        account.executeTransaction("withdraw", 500);  // withdraw
+        account.executeTransaction("withdraw", 500);  // invalid
+        System.out.println("Final Balance: $" + account.getBalance());
+    }
+}
