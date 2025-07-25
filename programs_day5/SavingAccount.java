910486e62667b84331b87f03e82f0fae5c3c6634
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-25 19:15:17 +0530

    Create SavingAccount.java

---
+++ programs_day5/SavingAccount.java
@@ -0,0 +1,35 @@
+package programs_day5;
+
+
+
+public class SavingAccount implements Bank {
+    private double balance;
+
+    public SavingAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds to withdraw $" + amount);
+        }
+    }
+
+    @Override
+    public void displayAccountType() {
+        System.out.println("Account Type: Savings Account");
+    }
+}
