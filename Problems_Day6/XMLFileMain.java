8e4b86b6c7e6be8c59a9eb28a797885f00b012fd
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-28 12:26:28 +0530

    Create XMLFileMain.java

---
+++ Problems_Day6/XMLFileMain.java
@@ -0,0 +1,36 @@
+package Problems_Day6;
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class XMLFileMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File f1 = new File("C:\\Users\\Sukendra Reddy\\Desktop\\xmlfile");
+		try {
+			BufferedReader br1 = new BufferedReader(new FileReader(f1));
+			String line = br1.readLine();
+			while(line!=null) {
+				System.out.println("XML File Content: "+line);
+				line = br1.readLine();
+			}
+			br1.close();
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File not found "+e.getMessage());
+		}
+		catch(IOException e) {
+			System.out.println(""+e.getMessage());
+		}
+		finally {
+			System.out.println("Program successfully executed");			
+		}
+	}
+
+}
