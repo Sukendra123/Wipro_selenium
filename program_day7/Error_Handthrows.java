de6b0ecce60f48d04f5113fdc58d14a51e043c72
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-28 22:15:36 +0530

    Create Error_Handthrows.java

---
+++ program_day7/Error_Handthrows.java
@@ -0,0 +1,25 @@
+package program_day7;
+
+public class Error_Handthrows {
+	/*static void checkAge(int age) {
+		if(age<18) {
+			throw new ArithemeticException("Acess denied: you must wait until 18 turned");
+		}
+		else {
+			System.out.println("Acess granted");
+		}
+	}
+	public static void main(String[] args) {
+		try {
+			int age = 5;
+			if(age<0) {
+				throw new IllegalAccessException("Age canot be negative");
+			}
+			System.out.println("Your Age is:"+age);
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("Exception occured: "+e.getMessage() );
+		}
+	}*/
+
+}
