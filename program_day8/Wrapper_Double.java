bbe38f03f0f04c6e5f7f941ab574bac44e677ad9
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-07-29 21:03:27 +0530

    Create Wrapper_Double.java

---
+++ program_day8/Wrapper_Double.java
@@ -0,0 +1,10 @@
+package program_day8;
+
+public class Wrapper_Double {
+public static void main(String[] args) {
+	String string ="2000";
+	int number =Integer.parseInt(string);
+	System.out.println("The String is :" +string);
+	System.out.println("The number is:"+number);
+}
+}
