551156dcd044848cf4cf7c2b332ac4ed53066f5a
Sukendra123 <129142098+Sukendra123@users.noreply.github.com>
2025-08-18 21:17:16 +0530

    Create innvocaationcounttime.java

---
+++ testDemo1/src/test/java/Day26/innvocaationcounttime.java
@@ -0,0 +1,25 @@
+package Day26;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class innvocaationcounttime {
+
+	@Test(invocationCount = 3)
+	public void testmethod1() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://randomuser.me/");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//li[@data-label='name']")).click();
+		System.out.println("User name: " + driver.findElement(By.id("user_value")).getText());
+
+		driver.findElement(By.xpath("//li[@data-label='email']")).click();
+		System.out.println("Email address: " + driver.findElement(By.id("user_value")).getText());
+		driver.quit();
+
+	}
+	}
